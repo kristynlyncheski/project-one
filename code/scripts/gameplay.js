679161b3817e626dd1adddd52ff755aa817f1b7b
kristynlyncheski <klyncheski@gmail.com>
2016-03-03 15:19:36 -0500

    project one jsrolling

---
+++ code/scripts/gameplay.js
@@ -0,0 +1,96 @@
+console.log("gameplay js is loaded...");
+
+var count = 0;
+var playerNumber = 1;
+
+
+
+startButton.addEventListener("click", function(){
+  openingPage.classList.add("hidden");
+  gamePage.classList.remove("hidden");
+  count += 1;
+  console.log("count",count);
+  startGame();
+});
+
+
+function startGame(){
+  console.log("the game has started...");
+
+  rollButton.addEventListener("click", function(){
+    if (count <= 4){
+      console.log("roll cta clicked");
+      count += 1;
+      console.log("count in startGame",count);
+      rollDice();
+    }
+  });
+  clickDiceInit();
+  return count;
+}
+
+function rollDice(){
+  notClicked = document.querySelectorAll(".notclicked");
+  console.log("notClicked",notClicked);
+  if(notClicked){
+    rollRandom();
+  }
+  return notClicked;
+}
+
+function rollRandom(){
+  console.log("dice have been rolled...");
+
+  for (var i = 0; i < notClicked.length; i++){
+    var randomDieRoll = Math.ceil(6 * Math.random());
+    console.log("randomDieRoll is", randomDieRoll);
+    for (var die in diceObj) {
+      if (randomDieRoll === diceObj[die].dieValue){
+        notClicked[i].src = diceObj[die].img;
+      }
+    }
+  }
+  rollCount();
+}
+
+function rollCount() {
+  if (count <= 4) {
+    console.log("count in rollCount", count);
+    rollNumber.innerText = "Roll #" + count;
+    if (count === 4){
+      rollNumber.innerText = "Choose A Play";
+    }
+  } else {
+    if (playerNumber === 1){
+      playerNumber = 2;
+    } else if (playerNumber === 2) {
+      playerNumber = 1;
+    }
+    playersTurn.innerText = playerNumber;
+
+    rollNumber.innerText = "Player " + playerNumber + "'s Turn";
+    console.log("count is more than 4");
+    nextPlayer();
+    count = 0;
+  }
+  return count;
+}
+
+function clickDiceInit(){
+  for (var i  = 0; i < rollDieArray.length; i++){
+    rollDieArray[i].addEventListener("click", function(){
+      console.log("this in clickDiceInit",this);
+      this.classList.toggle("clicked");
+      this.classList.toggle("notclicked");
+    });
+  }
+}
+
+function nextPlayer(){
+  console.log("nextPlayer has been called...")
+  for (var i = 0; i < rollDieArray.length; i++){
+    rollDieArray[i].src = "./imgs/die0.png";
+    rollDieArray[i].classList.remove("clicked");
+    rollDieArray[i].classList.add("notclicked");
+  }
+}
--- code/scripts/global.js
+++ code/scripts/global.js
@@ -1,1 +1,55 @@
 console.log("global js loaded...");
+
+var openingPage = document.querySelector(".opening-page");
+var startButton = document.querySelector("button[name=start]");
+
+var gamePage = document.querySelector(".game");
+
+var playersTurn = document.querySelector(".player-number");
+
+var rollButton = document.querySelector("button[name=roll]");
+var rollNumber = document.querySelector(".roll-number");
+
+var resultsPage = document.querySelector(".results-page");
+
+
+var roll1 = document.querySelector(".roll-one .rolled-die-imgs");
+var roll2 = document.querySelector(".roll-two .rolled-die-imgs");
+var roll3 = document.querySelector(".roll-three .rolled-die-imgs");
+var roll4 = document.querySelector(".roll-four .rolled-die-imgs");
+var roll5 = document.querySelector(".roll-five .rolled-die-imgs");
+
+var rollDieArray = [roll1,roll2,roll3,roll4,roll5];
+
+var notClicked;
+
+var diceObj = {
+  die0: {
+    img: "./imgs/die0.png",
+    dieValue: 0,
+  },
+  die1: {
+    img: "./imgs/die1.png",
+    dieValue: 1,
+  },
+  die2: {
+    img: "./imgs/die2.png",
+    dieValue: 2,
+  },
+  die3: {
+    img: "./imgs/die3.png",
+    dieValue: 3,
+  },
+  die4: {
+    img: "./imgs/die4.png",
+    dieValue: 4,
+  },
+  die5: {
+    img: "./imgs/die5.png",
+    dieValue: 5,
+  },
+  die6: {
+    img: "./imgs/die6.png",
+    dieValue: 6,
+  },
+};
